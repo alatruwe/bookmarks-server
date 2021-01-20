9ff8f5d4331a338408b6d477794ac8f5d4606708
adelinel <adeline.latruwe@gmail.com>
2021-01-19 22:29:31 -0800

    added route handler for DELETE /bookmarks/:id

--- src/bookmarks-router.js
+++ src/bookmarks-router.js
@@ -49,16 +49,33 @@ bookmarksRouter
       .json({ id });
   });
 
-bookmarksRouter.route("/bookmarks/:id").get((req, res) => {
-  const { id } = req.params;
-  const bookmark = bookmarks.find((book) => book.id == id);
+bookmarksRouter
+  .route("/bookmarks/:id")
+  .get((req, res) => {
+    const { id } = req.params;
+    const bookmark = bookmarks.find((book) => book.id == id);
 
-  if (!bookmark) {
-    logger.error(`Bookmark with id ${id} not found.`);
-    return res.status(404).send("Bookmark Not Found");
-  }
+    if (!bookmark) {
+      logger.error(`Bookmark with id ${id} not found.`);
+      return res.status(404).send("Bookmark Not Found");
+    }
 
-  res.json(bookmark);
-});
+    res.json(bookmark);
+  })
+  .delete((req, res) => {
+    const { id } = req.params;
+
+    const bookmarkIndex = bookmarks.findIndex((book) => book.id == id);
+
+    if (bookmarkIndex === -1) {
+      logger.error(`Bookmark with id ${id} not found.`);
+      return res.status(404).send("Not Found");
+    }
+
+    bookmarks.splice(bookmarkIndex, 1);
+
+    logger.info(`Bookmark with id ${id} deleted.`);
+    res.status(204).end();
+  });
 
 module.exports = bookmarksRouter;
