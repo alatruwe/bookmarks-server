1ea85f1e9778229829ac14dc71614ee23c97b537
adelinel <adeline.latruwe@gmail.com>
2021-01-19 22:11:31 -0800

    added route handler for GET /bookmarks/:id

--- src/bookmarks-router.js
+++ src/bookmarks-router.js
@@ -1,6 +1,6 @@
 const express = require("express");
 //const { v4: uuid } = require("uuid");
-//const logger = require("./logger");
+const logger = require("./logger");
 const { bookmarks } = require("./store");
 
 const bookmarksRouter = express.Router();
@@ -8,6 +8,18 @@ const bookmarksRouter = express.Router();
 
 bookmarksRouter.route("/bookmarks").get((req, res) => {
   res.json(bookmarks);
+});
+
+bookmarksRouter.route("/bookmarks/:id").get((req, res) => {
+  const { id } = req.params;
+  const bookmark = bookmarks.find((book) => book.id == id);
+
+  if (!bookmark) {
+    logger.error(`Bookmark with id ${id} not found.`);
+    return res.status(404).send("Bookmark Not Found");
+  }
+
+  res.json(bookmark);
 });
 
 module.exports = bookmarksRouter;
