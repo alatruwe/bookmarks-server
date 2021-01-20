68b3edf016691bf7b88f33ee65a21352ee56c9eb
adelinel <adeline.latruwe@gmail.com>
2021-01-19 22:22:05 -0800

    added route handler for POST /bookmarks

--- src/bookmarks-router.js
+++ src/bookmarks-router.js
@@ -1,14 +1,53 @@
 const express = require("express");
-//const { v4: uuid } = require("uuid");
+const { v4: uuid } = require("uuid");
 const logger = require("./logger");
 const { bookmarks } = require("./store");
 
 const bookmarksRouter = express.Router();
-//const bodyParser = express.json();
+const bodyParser = express.json();
 
-bookmarksRouter.route("/bookmarks").get((req, res) => {
-  res.json(bookmarks);
-});
+bookmarksRouter
+  .route("/bookmarks")
+  .get((req, res) => {
+    res.json(bookmarks);
+  })
+  .post(bodyParser, (req, res) => {
+    const { title, url, rating, description } = req.body;
+
+    if (!title) {
+      logger.error(`title is required`);
+      return res.status(400).send("Invalid data");
+    }
+    if (!url) {
+      logger.error(`title is required`);
+      return res.status(400).send("Invalid data");
+    }
+    if (!rating) {
+      logger.error(`title is required`);
+      return res.status(400).send("Invalid data");
+    }
+    if (!description) {
+      logger.error(`title is required`);
+      return res.status(400).send("Invalid data");
+    }
+
+    const id = uuid();
+
+    const bookmark = {
+      id,
+      title,
+      url,
+      rating,
+      description,
+    };
+
+    bookmarks.push(bookmark);
+    logger.info(`Bookmark with id ${id} created`);
+    res
+      .status(201)
+      .location(`http://localhost:8000/bookmarks/${id}`)
+      .json({ id });
+  });
 
 bookmarksRouter.route("/bookmarks/:id").get((req, res) => {
   const { id } = req.params;
