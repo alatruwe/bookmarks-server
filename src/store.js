59af17c1577b6c574e6346759f2badbfa6a37861
adelinel <adeline.latruwe@gmail.com>
2021-01-19 21:49:10 -0800

    added mock data

---
+++ src/store.js
@@ -0,0 +1,16 @@
+const bookmarks = [
+  {
+    title: "Google",
+    url: "http://www.google.com",
+    rating: "3",
+    description: "No evil",
+  },
+  {
+    title: "Google",
+    url: "http://www.google.com",
+    rating: "3",
+    description: "No evil",
+  },
+];
+
+module.exports = { bookmarks };
