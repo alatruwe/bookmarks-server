90818efa6c8d1200bb16bc0465a1cb4faf59a267
adelinel <adeline.latruwe@gmail.com>
2021-01-22 13:48:31 -0800

    added mock data in database

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,16 @@
+INSERT INTO bookmarks (id, title, url, rating, description)
+VALUES
+  ('1', 'Google',
+    'http://www.google.com', '4', 'Bla bla bla1'),
+  ('2', 'Google',
+    'http://www.google.com', '5', 'Bla bla bla2'),
+  ('3', 'Google',
+    'http://www.google.com', '1', 'Bla bla bla3'),
+  ('4', 'Google',
+    'http://www.google.com', '2', 'Bla bla bla4'),
+  ('5', 'Google',
+    'http://www.google.com', '3', 'Bla bla bla5'),
+  ('6', 'Google',
+    'http://www.google.com', '4', 'Bla bla bla6');
+  
+ 
